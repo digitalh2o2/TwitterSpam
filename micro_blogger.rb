6ab9bad6770b2b0eda060cd450669ea5d5268499
Saul Ocampo <digitalh2o@gmail.com>
2016-02-18 23:11:47 -0600

    Working on Twitter bot

---
+++ micro_blogger.rb
@@ -0,0 +1,69 @@
+require 'jumpstart_auth'
+
+class MicroBlogger
+	attr_reader :client
+
+	def initialize
+		puts "Initializing MicroBlogger"
+		@client = JumpstartAuth.twitter
+	end
+
+	def tweet(message)
+		if message.length <= 140
+			puts @client.update(message) 
+		else 
+			puts "Too many characters!"
+		end
+	end
+
+	def dm(target, message)
+		screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+		if screen_names.include?(target)
+			puts "Trying to send #{target} this direct message"
+			puts message
+			message = "d @#{target} #{message}"
+			tweet(message)		
+		else
+			puts "You can only Direct Message your followers!"
+		end
+	end
+
+	def followers_list
+		screen_names = []
+		@client.followers.each do |follower|
+			screen_names << @client.user(follower).screen_name
+		end
+		return screen_names
+	end
+
+	def spam_my_followers(message)
+		followers = followers_list
+		followers.each do |follower|
+			dm(follower, message)
+		end		
+	end
+
+	def run
+		puts "Weclome to the JSL Twitter Client!"
+		command = ""
+		while command != "q"
+			printf "enter command: "
+			input = gets.chomp
+			parts = input.split(" ")
+			command = parts[0]
+
+			case command
+			when "q" then puts "Goodbye!"
+			when "t" then tweet(parts[1..-1].join(" "))
+			when "dm" then dm(parts[1], parts[2..-1].join(" "))
+			when "spam" then spam_my_followers(parts[1..-1].join(" "))
+			else
+				puts "Sorry, I don't know how to #{command}"
+			end
+		end
+	end
+end
+
+blogger = MicroBlogger.new
+blogger.run
+
